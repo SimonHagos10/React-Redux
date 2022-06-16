3565fce2003fb7bfc6640f253cee3fe743fe77c2
Simon Hagos <Simon10Hagos50@gmail.com>
2022-06-16 18:45:21 -0500

    campsitesSlice

---
+++ src/features/campsites/CampsitesList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Col, Row} from 'reactstrap';
+import CampsiteCard from "./CampsiteCard";
+import { selectAllCampsites } from './campsitesSlice';
+
+function CampsitesList() {
+    const campsites = selectAllCampsites();
+
+    return (
+        <Row className='ms-auto'>
+            {campsites.map ((campsite) => (
+                <Col md='5' className='m-4' key={campsite.id}>
+                    <CampsiteCard campsite={campsite} />
+                </Col>
+            ))}
+        </Row>
+    );
+}
+
+export default CampsitesList;
