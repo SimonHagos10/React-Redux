c2fdba4bf308c6f934cc776e51bdeccef0d60a31
Simon Hagos <Simon10Hagos50@gmail.com>
2022-07-01 00:36:48 -0500

    Redux for All

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,4 +1,16 @@
 import { COMMENTS } from "../../app/shared/COMMENTS";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    commentsArray: COMMENTS
+};
+
+const commentsSlice = createSlice({
+    name: 'comments',
+    initialState
+});
+
+export const commentsReducer = commentsSlice.reducer;
 
 export const selectCommentsByCampsiteId = (campsiteId) => {
     return COMMENTS.filter(
